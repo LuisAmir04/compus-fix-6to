1f67f393a1a776cbe742783eff1a18fee2991f18
LuisAmir04 <luisamirmendeztrabajos@gmail.com>
2026-06-19 16:06:22 -0500

    Propuesta de pagina

--- www/js/login.js
+++ www/js/login.js
@@ -5,7 +5,7 @@ const password = document.querySelector("#password_field");
 button.addEventListener("click", e => {
   e.preventDefault();
   
-  fetch("../php/users.php", {
+  fetch("php/users.php", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -22,10 +22,10 @@ button.addEventListener("click", e => {
 
       localStorage.setItem("user", JSON.stringify(json.data));
       
-      if(json.data.id_role === 1) {
-          window.location.href = "../sales/"; // Vista de administrador/órdenes
+if(json.data.id_role === 1) {
+          window.location.href = "app/admin.html"; 
       } else {
-          window.location.href = "../repair_orders/"; // Vista de técnico
+          window.location.href = "app/tecnico.html"; 
       }
     } else {
       alert("Credenciales incorrectas.");
